3ae6212f49e5881b53930c1f2432487c1a482737
pcbzmani <pcbzmani@gmail.com>
2020-03-30 11:49:13 +0530

    Code to solve Puzzle

---
+++ code.py
@@ -0,0 +1,8 @@
+print("Enter the Sum of Horizontal three Numbers :")
+abc = int(input())
+print("Enter the Sum of Veritical three Numbers :")
+dbe = int(input())
+print("Enter the Sum of all  Numbers except Middle number :")
+abde = int(input())
+b=((abc+dbe) - abde)/2
+print(f"Middle number is:  {b} " )
